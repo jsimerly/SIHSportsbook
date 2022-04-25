d51f45584004f1e3075f6bac581d7bad7dc4318b
Jacob <simerly81@gmail.com>
2022-04-25 16:34:51 -0400

    update migration

--- Betting/urls.py
+++ Betting/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('create-league/', views.CreateLeague.as_view(), name='create-league')
 ]
--- Betting/views.py
+++ Betting/views.py
@@ -1,3 +1,9 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class CreateLeague(APIView):
+    def post(self, request, format='json'):
+        print('posted')
+        return Response(status=status.HTTP_200_OK)
