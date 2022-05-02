06c27f1aa3609e51ed1a7d976175afe7d70422ee
Jacob <simerly81@gmail.com>
2022-05-01 23:19:14 -0400

    close

--- Betting/urls.py
+++ Betting/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create-league/', views.CreateLeague.as_view(), name='create-league'),
     path('update-player-info/', views.UpdateNflPlayers.as_view(), name='update-player-info'),
     path('update-all-projections', views.UpdatePlayerProjections.as_view()),
+    path('update-league-projections', views.UpdateLeagueProjections.as_view()),
 ]
