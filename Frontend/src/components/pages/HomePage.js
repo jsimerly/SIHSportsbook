8da1cfb879d54b8ffb3dcf25a6c09b41920584d9
Jacob <simerly81@gmail.com>
2022-05-16 20:35:21 -0400

    bet selection is wokring!!!1

--- Frontend/src/components/pages/HomePage.js
+++ Frontend/src/components/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { render } from "react-dom";
 import DrawerComp from "../organisms/Drawer";
 import Matchups from "../organisms/Matchups";
@@ -11,6 +11,64 @@ import {
 
 
 export default function App(props){
+    const [selectedBets, setSelectedBets] = useState([])
+
+    const matchupsJson = [
+        {   id: 1,
+            matchupId: 1,
+            payoutDate: "Tues 3:00 AM",
+            vig: 0.05,
+            data: { 
+                team1: "Sugma",
+                team2: "'Member Berries",
+                over: "220",
+                under: "220",
+                sp_pos: "+3.5",
+                sp_neg: "-3.5",
+                ml_pos: +210,
+                ml_neg: -180
+            },
+        },
+        {   id: 2,
+            matchupId: 1,
+            payoutDate: "Tues 3:00 AM",
+            vig: 0.05,
+            data: { 
+                team1: "Team 3",
+                team2: "Team 4",
+                over: "120",
+                under: "120",
+                sp_pos: "+5.5",
+                sp_neg: "-5.5",
+                ml_pos: 115,
+                ml_neg: -129,
+            }
+        },
+        {   id: 3,
+            matchupId: 1,
+            payoutDate: "Tues 3:00 AM",
+            vig: 0.05,
+            data: { 
+                team1: "Team 5",
+                team2: "Team 6",
+                over: "120",
+                under: "120",
+                sp_pos: "+5.5",
+                sp_neg: "-5.5",
+                ml_pos: 115,
+                ml_neg: -129
+            }
+        },
+    ]
+
+    function handleBetSelected(newBetList) {
+        // const betType = newBet.betData.betType
+        // const betValue = newBet.matchupData.data[betType]
+     
+        setSelectedBets(newBetList)
+        console.log(selectedBets)
+    }
+
     return (
         <div>
             <Grid container>
@@ -20,10 +78,10 @@ export default function App(props){
                     </Box>
                 </Grid>
                 <Grid margin={2} md={7} spacing={1} sx={{border:1}}>
-                    <Matchups/>
+                    <Matchups data={matchupsJson} betHandler={handleBetSelected}/>
                 </Grid>
                 <Grid margin={2} md={2} spacing={1} sx={{border:1}}>
-                    <BetSlip/>
+                    <BetSlip data={selectedBets}/>
                 </Grid>
             </Grid>
         </div>
