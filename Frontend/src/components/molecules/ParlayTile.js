e4bd64cd7a17b8590392b32b8d5e51a6bbcbaa12
Jacob <simerly81@gmail.com>
2022-05-20 14:37:44 -0400

    parlay toggling

---
+++ Frontend/src/components/molecules/ParlayTile.js
@@ -0,0 +1,16 @@
+import React, { Component, useState } from "react";
+import { 
+    Grid,
+    Box,
+    TextField,
+    IconButton,
+} from "@mui/material";
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+
+
+export default function ParlayTile(props){
+
+    return (
+        <div> THIS IS A PARLAY TILE </div>
+    )
+ }
