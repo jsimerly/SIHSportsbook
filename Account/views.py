322fd834cbd7eb3bec099810e19f9fd6487e9056
Jacob <simerly81@gmail.com>
2022-06-21 22:56:21 -0400

    Email Verification Working

--- Account/views.py
+++ Account/views.py
@@ -7,6 +7,7 @@ from .serializers import *
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode   
 from django.utils.encoding import force_bytes, force_str
+from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.conf import settings
 
@@ -16,19 +17,24 @@ from .utils import generate_token
 def send_action_email(user, request):
     current_site = get_current_site(request)
     email_subject = 'Activate Your Account'
-    email_body = ('activation.html', {
-        'user' : user,
-        'domain' : current_site,
-        'uid' : urlsafe_base64_encode(force_bytes(user.id)),
-        'token' : generate_token.make_token(user)
-    })
+    email_body = render_to_string(
+        'activation.html', 
+        {
+            'user' : user,
+            'domain' : current_site,
+            'uid' : urlsafe_base64_encode(force_bytes(user.id)),
+            'token' : generate_token.make_token(user)
+        }
+    )
 
     email = EmailMessage(
         subject=email_subject,
         body=email_body,
         from_email=settings.EMAIL_FROM_USER,
-        to=[user.email]
+        to=[user.email],
     )
+
+    
 
     email.send()
 
