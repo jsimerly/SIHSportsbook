b8f4ffeb98d3f09daf053b12047f4f5e324433dc
Jacob <simerly81@gmail.com>
2022-07-19 22:38:51 -0400

    need to add 'team' to placed bets

--- Frontend/src/components/molecules/PlacedBets.js
+++ Frontend/src/components/molecules/PlacedBets.js
@@ -11,13 +11,35 @@ import CircleTwoToneIcon from '@mui/icons-material/CircleTwoTone';
 import RegBetSlipOpen from "../atoms/RegBetSlipOpen";
 
 export default function PlacedBets(props){
+    let betValue = 'Error'
+    let betType = 'Error'
+    let betStatus = 'Error'
+    let betStatusColor = 'grey'
+
+    if (props.bet.type === 'FFM'){
+        if (props.bet.bet_type === 'M1'){
+            betValue = props.bet.subtype_info.team1
+            betType = 'MONEYLINE'
+        } else if (props.bet.bet_type === 'M2'){
+            betValue = props.bet.subtype_info.team2
+            betType = 'MONEYLINE'
+        } else if (props.bet.bet_type === 'O') {
+            betType = 'Over'
+        } else if (props.bet.bet_type === 'U') {
+            betType = ''
+        } else if (props.bet.bet_type === 'O') {
+            betType = 'Over'
+        } else if (props.bet.bet_type === 'O') {
+            betType = 'Over'
+        }
+    }
     
     return (
         <RegBetSlipOpen
-            betValue = 'Jared Cannonier'
-            betType = 'MONEYLINE'
-            statusColor = 'grey'
-            status = 'OPEN'
+            betValue = {betValue}
+            betType = {betType}
+            statusColor = {betStatusColor}
+            status = {betStatus}
             event = 'Israel Adesanya vs Jared Cannonier'
             bet = {props.bet}
         />
