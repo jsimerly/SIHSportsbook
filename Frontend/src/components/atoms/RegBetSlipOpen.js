b1d031af75ec4f2692927a9d55d76d8bc540a66b
Jacob <simerly81@gmail.com>
2022-07-19 22:31:39 -0400

    Designed single bets

--- Frontend/src/components/atoms/RegBetSlipOpen.js
+++ Frontend/src/components/atoms/RegBetSlipOpen.js
@@ -9,7 +9,7 @@ import {
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CircleTwoToneIcon from '@mui/icons-material/CircleTwoTone';
 
-export default function OpenBetSlipReg(props){
+export default function RegBetSlipOpen(props){
     console.log('++++++++++++')
     console.log(props.bet)
     return (
@@ -30,22 +30,32 @@ export default function OpenBetSlipReg(props){
                                 {props.betType}
                             </Box>
                             <Box>
-                                {props.bet.wager}
+                                Wager: ${props.bet.wager.toFixed(2)}
+                            </Box>
+                            <Box>
+                                To Win: ${props.bet.payout_amount.toFixed(2)}
                             </Box>
                             
                         </Grid>
                     </Grid>
-              
                 </AccordionSummary>
                 <AccordionDetails>
                     <Grid container>
                         <Grid item xs={10}>
                             <Box>
-
+                                {props.status}
+                            </Box>
+                            <Box>
+                                {props.event}
+                            </Box>
+                            <Box>
+                                {props.bet.payout_date}
                             </Box>
                         </Grid>
-                        <Grid item xs={2}>
-                            {props.status}
+                        <Grid item xs={2} sx={{pl:1.5}}>
+                            <Box>
+                                {props.bet.line}
+                            </Box>
                         </Grid>
                     </Grid>
                 </AccordionDetails>
--- Frontend/src/components/molecules/PlacedBets.js
+++ Frontend/src/components/molecules/PlacedBets.js
@@ -8,15 +8,17 @@ import {
 } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CircleTwoToneIcon from '@mui/icons-material/CircleTwoTone';
-import OpenBetSlipReg from "../atoms/openBetSlipReg";
+import RegBetSlipOpen from "../atoms/RegBetSlipOpen";
 
 export default function PlacedBets(props){
     
     return (
-        <OpenBetSlipReg 
+        <RegBetSlipOpen
             betValue = 'Jared Cannonier'
             betType = 'MONEYLINE'
             statusColor = 'grey'
+            status = 'OPEN'
+            event = 'Israel Adesanya vs Jared Cannonier'
             bet = {props.bet}
         />
     )
